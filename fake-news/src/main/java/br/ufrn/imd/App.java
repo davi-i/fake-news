dcc8e212272b1d755d82b7253fa37baf85756926
davi-i <josedavifrancelino@gmail.com>
2022-10-16 17:46:08 -0300

    feat: added method to calculate average

--- fake-news/src/main/java/br/ufrn/imd/App.java
+++ fake-news/src/main/java/br/ufrn/imd/App.java
@@ -4,10 +4,8 @@ import br.ufrn.imd.algorithm.CosineAlgorithm;
 import br.ufrn.imd.algorithm.JaroWinklerAlgorithm;
 import br.ufrn.imd.algorithm.LevenshteinAlgorithm;
 
-/**
- * Hello world!
- *
- */
+import java.util.List;
+
 public class App 
 {
     public static void main( String[] args )
@@ -16,12 +14,19 @@ public class App
         var jaroWinkler = new JaroWinklerAlgorithm();
         var levenshtein = new LevenshteinAlgorithm();
 
-        String left = "bolsonaro forjou a facada";
+        var list = List.of("antes apenas bandido bolsonaro brasil campos carteirinha contatos contra corrente demais desmascarar enviar esclarecidos essa este folha incautos inteiro jornalista materia mello minuto para patricia petista precisam quebre seja tarde voce", "a terra e plana", "tomar cafe evita cancer");
+
+        String left = "a terra e plana";
         String right = "bolsonaro não forjou a facada";
 
         System.out.println("\"" + left + "\" e \"" + right + "\":");
         System.out.println( "Cosine: " + cosine.calculate(left, right) );
         System.out.println( "Jaro-Winkler: " + jaroWinkler.calculate(left, right) );
         System.out.println( "Levenshtein: " + levenshtein.calculate(left, right) );
+
+        System.out.println("\"" + left +"\" comparado com a lista:");
+        System.out.println( "Cosine: " + cosine.average(left, list) );
+        System.out.println( "Jaro-Winkler: " + jaroWinkler.average(left, list) );
+        System.out.println( "Levenshtein: " + levenshtein.average(left, list) );
     }
 }
--- fake-news/src/main/java/br/ufrn/imd/algoritm/Algorithm.java
+++ fake-news/src/main/java/br/ufrn/imd/algoritm/Algorithm.java
@@ -1,5 +1,14 @@
 package br.ufrn.imd.algorithm;
 
+import java.util.Collection;
+
 public interface Algorithm {
     public abstract Double calculate(CharSequence left, CharSequence right);
+
+    public default Double average(CharSequence left, Collection<String> rights) {
+        return rights.stream()
+                     .mapToDouble(right -> this.calculate(left, right))
+                     .average()
+                     .orElse(0);
+    }
 }
