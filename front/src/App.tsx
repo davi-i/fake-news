1a2f6ca4a95a65c7c217439aa2a88288b04ba1ce
Fernando Gomes de Almeida <fernandogdalmeida@gmail.com>
2022-12-10 20:19:36 -0300

    front modified

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,44 +1,90 @@
 import './App.css'
+import { Children, useState } from 'react'
+
+var showChar = 120;
+
+type News = {
+  content: string;
+  algorithm: "cosine" | "jaro-winkler" | "levenshtein";
+  percentage: number;
+}
+
+let news: News[] = [{ content: "bla", algorithm: "cosine", percentage: 50},{ content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec eros imperdiet, suscipit ipsum ac, efficitur turpis. Sed ultrices hendrerit libero, laoreet lacinia erat pretium ac.", algorithm: "jaro-winkler", percentage: 50}];
+
+const ReadMore: React.FC<{ children: string }> = ({ children }) => {
+  const text = children;
+  const size = children.length;
+  const [isReadMore, setIsReadMore] = useState(true);
+
+  const toggleReadMore = () => {
+    setIsReadMore(!isReadMore);
+  };
+
+  if (size <= showChar) {
+    return (<td> {text} </td>);
+  } else {
+    return (
+      <td>
+        {isReadMore ? text.substring(0, showChar) : text}
+        <span onClick={toggleReadMore} className="read-or-hide">
+          {isReadMore ? " read more [+]" : " show less"}
+        </span>
+      </td>
+    )
+  }
+};
+
+const UploadFile = () => {
+  document.getElementById("uploadFile")?.click();
+}
 
 function App() {
   return (
     <div className="App">
-      <h1>Fake News Detector</h1>
-      <div class="detector">
-        <span>insert your text here:</span>
+      <h1><b>Fake News Detector</b></h1>
+      <div className="detector">
+        <span>Insert your text:</span>
 
-        <textarea></textarea>
+        <textarea placeholder="Your text goes here"></textarea>
 
-        <span className="center">or</span>
+        <span className="center">OR</span>
 
-        <button className='primary'>upload csv file</button>
+        <button className='primary' onClick={UploadFile}>upload csv file</button>
+        <input id="uploadFile" type="file" accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel" hidden/>
 
         <hr />
 
-        <span>select an algorithm:</span>
+        <span>Select an Algorithm:</span>
 
 
         <select>
-          <option disabled selected>select an algorithm</option>
-          <option value={0}>bla bla</option>
-          <option value={1}>ble ble</option>
-          <option value={2}>bli bli</option>
+          <option disabled selected>-- select an algorithm --</option>
+          <option value={0}>Cosine Algorithm</option>
+          <option value={1}>Jaro-Winkler Algorithm</option>
+          <option value={2}>Levenshtein Distance Algorithm</option>
         </select>
 
         <hr />
 
-        <button className='secondary'>find the truth!</button>
+        <button className='secondary'>Find The Truth!</button>
       </div>
-
-      <span>history</span>
-
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec eros imperdiet, suscipit ipsum ac, efficitur turpis. Sed ultrices hendrerit libero, laoreet lacinia erat pretium ac.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec eros imperdiet, suscipit ipsum ac, efficitur turpis. Sed ultrices hendrerit libero, laoreet lacinia erat pretium ac.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec eros imperdiet, suscipit ipsum ac, efficitur turpis. Sed ultrices hendrerit libero, laoreet lacinia erat pretium ac.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec eros imperdiet, suscipit ipsum ac, efficitur turpis. Sed ultrices hendrerit libero, laoreet lacinia erat pretium ac.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec eros imperdiet, suscipit ipsum ac, efficitur turpis. Sed ultrices hendrerit libero, laoreet lacinia erat pretium ac.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec eros imperdiet, suscipit ipsum ac, efficitur turpis. Sed ultrices hendrerit libero, laoreet lacinia erat pretium ac.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec eros imperdiet, suscipit ipsum ac, efficitur turpis. Sed ultrices hendrerit libero, laoreet lacinia erat pretium ac.</p>
+      <br/><br/>
+      <table>
+        <caption>History</caption>
+        <br/>
+        <tr>
+          <th>News</th>
+          <th>Algorithm</th>
+          <th>Percentage (%)</th>
+        </tr>
+        {news.map((news) => (
+        <tr>
+          <ReadMore>{news.content}</ReadMore>
+          <td>{news.algorithm}</td>
+          <td>{news.percentage}</td>
+        </tr>
+        ))}       
+      </table>
     </div>
   )
 }
